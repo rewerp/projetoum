292127431af14ff6060b20a820363492279c912e
carlos-dev-dobes <carlos.alfredo@dobesone.com.br>
2021-08-12 16:37:07 -0300

    Commit inicial
    Commit inicial do projeto react

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonIncrement } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Entrar</Button>
+      </div>
+      <div>
+        <ButtonIncrement />
+      </div>
+    </div>
+  );
+}
+
+export default App;
