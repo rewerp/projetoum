6406fc7ebbfde38d9bb31173ac4058a282f6efc6
carlos-dev-dobes <carlos.alfredo@dobesone.com.br>
2021-08-13 08:57:52 -0300

    Adicionar arquivos ao projeto
    Adicionar arquivos ao projeto

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
 type ButtonProps = {
-    //    text?: string;
-    //    text?: string[];      //Tipar como um array de String, opcao 1
-    //    text?: Array<string>; //Tipar como um array de String, opcao 2
-    children?: string; //Tipar um componentes como children para utilizar como uma tag HTML
+  //    text?: string;
+  //    text?: string[];      //Tipar como um array de String, opcao 1
+  //    text?: Array<string>; //Tipar como um array de String, opcao 2
+  children?: string; //Tipar um componentes como children para utilizar como uma tag HTML
 }
 
 export function Button(props: ButtonProps) {
-    return (
-        <button>{props.children || `Clique aqui`}</button>
-    )
+  return (
+    <button>{props.children || `Clique aqui`}</button>
+  )
 }
 
 export function ButtonIncrement() {
-    // let counter = 0;
-    const [counter, setCounter] = useState(0)
+  // let counter = 0;
+  const [counter, setCounter] = useState(0)
 
-    function increment() {
-        // counter ++; //uso com variavel let
-        setCounter(counter + 1);
-        // console.log(counter);
-    }
+  function increment() {
+    // counter ++; //uso com variavel let
+    setCounter(counter + 1);
+    // console.log(counter);
+  }
 
-    return (
-        <button onClick={increment}>
-            {counter}
-        </button>
-    )
+  return (
+    <button onClick={increment}>
+      {counter}
+    </button>
+  )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 export Home() {
-    return (
-        <div>
-            <aside>
-                <img scr="{}" alt="" />       
-            </aside>
-        </div>
-    )
+  return (
+    <div>
+      <aside>
+        <img scr="{}" alt="" />
+      </aside>
+    </div>
+  )
 }
